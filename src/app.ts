1b81fb321b56d8c9489e2ddc668ed80dbd7a30eb
omerihsan <momerihsan@outlook.com>
2022-12-18 16:43:15 +0500

    config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 export default class ExpressApplication {
   private readonly app: express.Application;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('123123Hello World!123');
+app.get("/", (req, res) => {
+  res.send("123123Hello World!123");
 });
 
-app.listen(port, () =>
-  console.log(`123Express is listening at http://localhost:${port}`)
-);
-
-// bad
-let count = 1;
-if (true) {
-  count += 1;
-}
+app.listen(port, () => console.log(`123Express is listening at http://localhost:${port}`));
