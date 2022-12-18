035871722a454ce799e5de7b9b7dcb8526aeb51d
omerihsan <momerihsan@outlook.com>
2022-12-18 22:29:49 +0500

    remove airbnb config

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,17 @@ import express from "express";
 export default class ExpressApplication {
   private readonly app: express.Application;
 
+  #a = "abc";
+
   constructor() {
     this.app = express();
+  }
+
+  private static test(a: string): void {
+    console.log("abc", a);
+  }
+
+  private static test1(): void {
+    console.log("abc");
   }
 }
